f4b8c7ca6654db23ac45dc6d430b0b10415c04c2
Max Richter <jim-x@web.de>
2020-11-07 17:07:59 +0100

    fix: v4

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-#Prerequesits
+# - Prerequesits -
 echo "-- Installing git --"
-if [ "$(which $1)" != "" ]; then
-    apt-get install $1 -y > /dev/null
+if [ "$(which git)" != "" ]; then
+    apt-get install git -y > /dev/null
 fi
 
-# Cloning Repo
+# - Cloning Repo - 
 echo "-- Cloning repo --"
 git clone git@github.com:jim-fx/.dotfiles.git ~/.dotfiles
 
-# Executing the script
+# - Executing the script -
 echo "-- starting script --"
 chmod +x ~/.dotfiles/setup.sh
 ~/.dotfiles/setup.sh
