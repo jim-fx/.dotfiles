59e9a15128f970a4f1905db2d3bf24b0156c5868
Jim Richter <jim-x@web.de>
2021-01-29 14:25:45 +0100

    feat: add folder creation to fx

--- configs/zsh/functions/fx.zsh
+++ configs/zsh/functions/fx.zsh
@@ -1,6 +1,17 @@
 # Helps navigating Sync directory
 function fx(){
+
   cd $HOME/SYNC/
+
+  if [ ! -d $2 ]; then
+      echo "dir $2 does not exists"
+      if read -q '?Do you want to create it (y/n)?'; then
+        mkdir $2
+      else
+        echo -e "\nalllrighty then, byyye"
+        return
+      fi
+  fi
 
   if [ "$2" != "" ]; then
     cd $2
