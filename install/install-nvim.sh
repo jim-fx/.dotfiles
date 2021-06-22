9345245ca9dab7aefa65f29f628b430023b7c616
Jim Richter <jim-x@web.de>
2021-06-22 13:07:02 +0200

    fix: quitting install vim plugins

--- install/install-nvim.sh
+++ install/install-nvim.sh
@@ -12,4 +12,4 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 echo " - installing vim plugins"
-nvim --headless +PlugInstall +q
+nvim --headless +PlugInstall +qa
