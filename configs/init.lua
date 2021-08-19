89b6d532c458855591d00af5927e00c588ac9f5e
Jim Richter <jim-x@web.de>
2021-08-19 17:14:34 +0200

    fix: init.lua

--- configs/init.lua
+++ configs/init.lua
@@ -31,63 +31,63 @@ paq:setup({verbose=true}) {
   {url="git@github.com:akinsho/nvim-toggleterm.lua"}
 }
 
-local status = pcall(require, "material");
-if not(status) then
+if u.has_plugin("nvim-treesiter") then
   paq.install();
+else
+
+  -- Global options
+  o.number = true
+  o.tabstop = 2
+  o.shiftwidth = 2 -- Indents will have a width of 4
+  o.softtabstop = 2 -- Sets the number of columns for a TAB
+  o.expandtab = false -- Expand TABs to spaces
+  cmd [[set mouse=a]]
+  cmd [[set undofile]]
+
+  -- Apply Theme
+  o.syntax = "enable"
+  o.termguicolors = true
+  g.NERDTreeShowHidden = true
+  g.NERDTreeAutoDeleteBuffer = true
+  g.NERDTreeMinimalUI = true
+  g.NERDTreeDirArrows = true
+  g.hidden = true
+  g.material_theme_style = "ocean_community"
+  if u.has_plugin("material") then
+    cmd [[colorscheme material]]
+  end
+  -- Remove background color
+  cmd [[highlight Normal guibg=none]]
+  cmd [[highlight NonText guibg=none]]
+
+  -- KeyBindings
+  g.mapleader = " "
+  require "keymappings"
+
+  -- Treesitter config
+  require "nvim-treesitter.configs".setup {ensure_installed = {"bash", "svelte", "css", "svelte", "typescript", "javascript", "go", "lua", "yaml"}, highlight = {enable = true}}
+  -- Toggleterm / Lazygit setup
+  require "lazy-git"
+  -- Autocommands
+  u.create_augroup(
+    {
+      {"VimEnter", "*", "if (@% == '') | NERDTree | endif"},
+      {"BufEnter", "*", 'if (winnr("$") == 1 && exists("b:NERDTree") && b:NERDTree.isTabTree()) | q | endif'}
+    },
+    "Nerdtree"
+  )
+
+  -- Autocompletion Setup
+  require "autocomplete"
+
+  -- LSP Config
+
+  require "lsp-utils"
+
+  opt.completeopt = {"menuone", "noinsert", "noselect"}
+  opt.shortmess:append({c = true})
+
+  -- Autoformat
+  require "autoformatter"
+
 end
-
--- Global options
-o.number = true
-o.tabstop = 2
-o.shiftwidth = 2 -- Indents will have a width of 4
-o.softtabstop = 2 -- Sets the number of columns for a TAB
-o.expandtab = false -- Expand TABs to spaces
-cmd [[set mouse=a]]
-cmd [[set undofile]]
-
--- Apply Theme
-o.syntax = "enable"
-o.termguicolors = true
-g.NERDTreeShowHidden = true
-g.NERDTreeAutoDeleteBuffer = true
-g.NERDTreeMinimalUI = true
-g.NERDTreeDirArrows = true
-g.hidden = true
-g.material_theme_style = "ocean_community"
-if u.has_plugin("material") then
-  cmd [[colorscheme material]]
-end
--- Remove background color
-cmd [[highlight Normal guibg=none]]
-cmd [[highlight NonText guibg=none]]
-
--- KeyBindings
-g.mapleader = " "
-require "keymappings"
-
--- Treesitter config
-require "nvim-treesitter.configs".setup {ensure_installed = {"bash", "svelte", "css", "svelte", "typescript", "javascript", "xml", "go", "lua", "yaml"} highlight = {enable = true}}
--- Toggleterm / Lazygit setup
-require "lazy-git"
--- Autocommands
-u.create_augroup(
-  {
-    {"VimEnter", "*", "if (@% == '') | NERDTree | endif"},
-    {"BufEnter", "*", 'if (winnr("$") == 1 && exists("b:NERDTree") && b:NERDTree.isTabTree()) | q | endif'}
-  },
-  "Nerdtree"
-)
-
--- Autocompletion Setup
-require "autocomplete"
-
--- LSP Config
-
-require "lsp-utils"
-
-opt.completeopt = {"menuone", "noinsert", "noselect"}
-opt.shortmess:append({c = true})
-
--- Autoformat
-require "autoformatter"
-
