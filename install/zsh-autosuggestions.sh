93ce33871d53637877d11b9b0e7b7e25f5180258
Jim Richter <jim-x@web.de>
2021-08-19 17:11:25 +0200

    feat: optimize install

--- install/zsh-autosuggestions.sh
+++ install/zsh-autosuggestions.sh
@@ -1,3 +1,8 @@
-echo " - cloning github.com/zsh-users/zsh-autosuggestions into ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
-git clone --quiet https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions > /dev/null
-echo " - finished"
+FOLDER=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+if [ -s $FOLDER ]; then
+  echo " ✓ zsh-autosuggestions is already installed"
+else
+  echo " - cloning github.com/zsh-users/zsh-autosuggestions into $FOLDER"
+  git clone --quiet https://github.com/zsh-users/zsh-autosuggestions $FOLDER > /dev/null
+  echo " - finished"
+fi
