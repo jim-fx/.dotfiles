af373c9428f53f5cdbc6ce35c1ac71467cb8fa77
Jim Richter <jim-x@web.de>
2021-08-19 17:11:25 +0200

    feat: optimize install

--- install/p10k.sh
+++ install/p10k.sh
@@ -1,3 +1,8 @@
-echo " - cloning github.com/romkatv/powerlevel10k into ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
-git clone --quiet --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k > /dev/null
-echo " - finished"
+FOLDER=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+if [ -s $FOLDER ]; then
+  echo " ✓ powerlevel10k is already installed"
+else
+  echo " - cloning github.com/romkatv/powerlevel10k into $FOLDER"
+  git clone --quiet --depth=1 https://github.com/romkatv/powerlevel10k.git $FOLDER > /dev/null
+  echo " - finished"
+fi
