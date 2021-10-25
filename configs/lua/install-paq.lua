326f536e4123f10e8fb3010a8ed9c71c4a8d3811
Max Richter <jim-x@web.de>
2021-10-25 17:44:21 +0000

    feat: add i3 config

--- configs/lua/install-paq.lua
+++
@@ -1,7 +0,0 @@
-local fn = vim.fn
-
-local install_path = fn.stdpath('data') .. '/site/pack/paqs/start/paq-nvim'
-
-if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', '--depth=1', 'https://github.com/savq/paq-nvim.git', install_path})
-end
