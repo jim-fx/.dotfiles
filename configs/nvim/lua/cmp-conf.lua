041b6cd367db05547d101a420203d95d89dbf74d
Jim Richter <jim-x@web.de>
2022-01-26 20:42:58 +0100

    uff

--- configs/nvim/lua/cmp-conf.lua
+++ configs/nvim/lua/cmp-conf.lua
@@ -1,84 +1,99 @@
 -- luasnip setup
-local cmd = vim.cmd;
+local cmd = vim.cmd
 local luasnip = require "luasnip"
 local lspkind = require "lspkind"
 local cmp = require "cmp"
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 
 cmp.setup {
-  formatting = {
-    format = lspkind.cmp_format({with_text = true, menu = ({
-      buffer = "[Buffer]",
-      nvim_lsp = "[LSP]",
-      luasnip = "[LuaSnip]",
-      nvim_lua = "[Lua]",
-      latex_symbols = "[Latex]",
-    })}),
-  },
-  experimental = {
-    ghost_text = true,
-  },
-  completion = {
-    completeopt = "menu,menuone",
-  },
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end
-  },
-  mapping = {
-    ['<C-Leader>'] = cmp.mapping.complete(),
-    ["<Tab>"] = function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      else
-        fallback()
-      end
-    end,
-    ["<S-Tab>"] = function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      else
-        fallback()
-      end
-    end,
-    ['<CR>'] = cmp.mapping.confirm({
-        behavior = cmp.ConfirmBehavior.Replace,
-        select = true,
-    }),
-  },
-  sources = {
-    {name = "nvim-lua"},
-    {name = "nvim_lsp"},
-    {name = "luasnip"},
-		{name = "path"},
-    {name = "buffer"},
-    {name = "calc"}
-  }
+    formatting = {
+        format = lspkind.cmp_format(
+            {
+                with_text = true,
+                menu = ({
+                    buffer = "[Buffer]",
+                    nvim_lsp = "[LSP]",
+                    luasnip = "[LuaSnip]",
+                    nvim_lua = "[Lua]",
+                    latex_symbols = "[Latex]"
+                })
+            }
+        )
+    },
+    experimental = {
+        ghost_text = true
+    },
+    completion = {
+        completeopt = "menu,menuone"
+    },
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end
+    },
+    mapping = {
+        ["<C-Leader>"] = cmp.mapping.complete(),
+        ["<Tab>"] = function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            else
+                fallback()
+            end
+        end,
+        ["<S-Tab>"] = function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            else
+                fallback()
+            end
+        end,
+        ["<CR>"] = cmp.mapping.confirm(
+            {
+                behavior = cmp.ConfirmBehavior.Replace,
+                select = true
+            }
+        )
+    },
+    sources = {
+        {name = "nvim_lua"},
+        {name = "nvim_lsp"},
+        {name = "luasnip"},
+        {name = "path"},
+        {name = "buffer"},
+        {name = "calc"}
+    }
 }
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline('/', {
-  sources = {
-    { name = 'buffer' }
-  }
-})
+cmp.setup.cmdline(
+    "/",
+    {
+        sources = {
+            {name = "buffer"}
+        }
+    }
+)
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-    { name = 'cmdline' }
-  })
-})
-
+cmp.setup.cmdline(
+    ":",
+    {
+        sources = cmp.config.sources(
+            {
+                {name = "path"}
+            },
+            {
+                {name = "cmdline"}
+            }
+        )
+    }
+)
 
 -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
 -- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-require'lspconfig'.html.setup {
-  capabilities = capabilities
+local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+require "lspconfig".html.setup {
+    capabilities = capabilities
 }
 
-cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({ map_char = { tex = '' } }))
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({map_char = {tex = ""}}))
--- configs/nvim/lua/formatter-conf.lua
+++ configs/nvim/lua/formatter-conf.lua
@@ -1,8 +1,59 @@
 local cmd = vim.cmd
+local util = require "vim.lsp.util"
+local utils = require("utils")
+
+function tablelength(T)
+    local count = 0
+    for _ in pairs(T) do
+        count = count + 1
+    end
+    return count
+end
+
+local function select_client(method)
+    local all_clients = vim.tbl_values(vim.lsp.buf_get_clients())
+
+    local supported_client
+    local found_client = false
+    for _, client in ipairs(all_clients) do
+        local supports = client.supports_method(method)
+        if supports then
+            found_client = true
+            supported_client = client
+        end
+    end
+
+    if not found_client then
+        return nil
+    end
+
+    return supported_client
+end
+
+function FormattingSync()
+    vim.cmd("Neoformat")
+
+    --     local client = select_client("textDocument/formatting")
+    --     if client == nil then
+    --         vim.notify("No LSP Client with formatting connected")
+    --     end
+
+    --     vim.notify("Formatting with LSP")
+
+    --     local params = util.make_formatting_params({})
+    --     local result, err = client.request_sync("textDocument/formatting", params, 500, vim.api.nvim_get_current_buf())
+    --     if result and result.result then
+    --         util.apply_text_edits(result.result)
+    --     elseif err then
+    --         vim.notify("vim.lsp.buf.formatting_sync: " .. err, vim.log.levels.WARN)
+    --     end
+end
+
+vim.cmd [[command! Format lua FormattingSync()]]
 
 cmd [[
     augroup auto_format
       au!
-      au BufWritePre * Neoformat
+      au BufWritePre * lua FormattingSync()
     augroup END
 ]]
