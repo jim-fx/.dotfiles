de5bbfdd8fddb8796a8298770f2424aec2b6de9a
Max Richter <jim-x@web.de>
2022-02-02 14:28:36 +0100

    fix:add missing function in lsp conf

---
+++ configs/sway/lock_screen.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/sh
+
+swaylock \
+	--screenshots \
+	--indicator-radius 500 \
+	--effect-blur 7x5 \
+	--effect-pixelate 7 \
+	--ring-color ffffff \
+	--line-color 000000 \
+	--inside-color ffffff \
+	--separator-color 00000000 \
+	--grace 2 \
+	--fade-in 2
