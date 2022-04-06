a80b7c1145fea6cbd8494ad3c035c60c40ae37f9
Max Richter <jim-x@web.de>
2022-04-06 23:34:14 +0200

    feat: some stuff

--- configs/sway/toggle_gaps.sh
+++ configs/sway/toggle_gaps.sh
@@ -5,10 +5,10 @@ GAPS=3
 # How to get the current inner border from swaymsg
 #$ echo $(swaymsg -t get_config | jq ".config") | sed -n '/gaps\ inner/p' | sed 's/gaps\ inner//g' | xargs
 
-if [ "$(pgrep waybar)" != "" ]; then
-  killall waybar;
+if [ "$(ps cax | grep waybar$)" != "" ]; then
   swaymsg gaps outer all set 0;
   swaymsg gaps inner all set 0;
+  killall waybar;
 else
   swaymsg gaps outer all set $GAPS;
   swaymsg gaps inner all set $GAPS;
