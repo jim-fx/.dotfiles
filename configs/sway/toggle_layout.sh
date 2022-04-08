59784525c0b55742c61f4e960e1427b295c4bb6b
Max <jim-x@web.de>
2022-04-08 15:47:38 +0200

    feat: some cleanup

--- configs/sway/toggle_layout.sh
+++ configs/sway/toggle_layout.sh
@@ -2,8 +2,10 @@
 
 WORKSPACE=$(swaymsg -t get_workspaces --raw | jq '.[] | select(.focused == true)')
 
+
 WORKSPACE_ID=$(echo $WORKSPACE | jq ".name" | tr -d '"')
-WORKSPACE_MODE=$(echo $WORKSPACE | jq ".layout");
+WORKSPACE_MODE=$(swaymsg -t get_tree | jq 'recurse(.nodes[]) | select(.nodes[].focused == true).layout');
+echo "WORKSPACE: $WORKSPACE_MODE"
 
 MODE_SPLITH='"splith"'
 MODE_SPLITV='"splitv"'
@@ -18,8 +20,12 @@ if [ $WORKSPACE_MODE = $MODE_SPLITH ]; then
 	set_layout splitv
 elif [ $WORKSPACE_MODE = $MODE_SPLITV ]; then
 	set_layout tabbed
+# elif [ $WORKSPACE_MODE = $MODE_TABBED ]; then
+# 	set_layout stacking
 elif [ $WORKSPACE_MODE = $MODE_TABBED ]; then
-	set_layout stacking
-elif [ $WORKSPACE_MODE = $MODE_STACKED ]; then
 	set_layout splith
+fi
+
+if [ "$1" != "" ]; then
+  set_layout $1
 fi
