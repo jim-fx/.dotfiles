e75b75f0783faafdf680a66cb70409abef2ac057
Max <jim-x@web.de>
2022-05-03 19:25:32 +0200

    fix: commands in command center

--- configs/nvim/lua/configs/command-center.lua
+++ configs/nvim/lua/configs/command-center.lua
@@ -4,31 +4,32 @@ local noremap = { noremap = true }
 command_center.add({
   {
     description = "Search inside current buffer",
-    cmd = "Telescope current_buffer_fuzzy_find",
+    cmd = "<CMD>Telescope current_buffer_fuzzy_find<CR>",
     keybindings = { "n", "<leader>fl", noremap },
   }, {
     -- If no descirption is specified, command is used to replace descirption by default
     -- You can change this behavior in settigns
-    cmd = "Telescope find_files",
+    cmd = "<CMD>Telescope find_files<CR>",
   },
   {
     -- If no keybindings specified, no keybindings will be displayed or registered
     description = "Find hidden files",
-    cmd = "Telescope find_files hidden=true",
+    cmd = "<CMD>Telescope find_files hidden=true<CR>",
   },
   {
     -- You can specify multiple keybindings for the same command ...
     description = "Show document symbols",
-    cmd = "Telescope lsp_document_symbols",
+    cmd = "<CMD>Telescope lsp_document_symbols<CR>",
   },
   {
     description = "Show function signaure (hover)",
-    cmd = "lua vim.lsp.buf.hover()",
+    cmd = "<CMD>lua vim.lsp.buf.hover()<CR>",
   }, {
     description = "(TS) Organize Imports",
-    cmd = "OrganizeImports"
+    cmd = "<CMD>OrganizeImports<CR>"
   }, {
     description = "ZenMode",
-    cmd = "<CMD>ZenMode<CR>"
+    cmd = "<CMD>ZenMode<CR>",
+    keybindings = { "n", "z", noremap }
   }
 })
