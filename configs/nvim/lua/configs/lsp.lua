c688f2c01cf9ee55f56f0d1419535b5dc3845a2c
Max <jim-x@web.de>
2022-06-23 00:20:19 +0200

    fix: cursor shapes in nested tmux

--- configs/nvim/lua/configs/lsp.lua
+++ configs/nvim/lua/configs/lsp.lua
@@ -11,11 +11,29 @@ table.insert(runtime_path, "lua/?/init.lua")
 local function on_attach(client, bufnr)
   local cap = client.server_capabilities
 
+
+  vim.notify(client.name)
+
   if (cap.documentFormattingProvider) then
     vim.api.nvim_create_autocmd("BufWritePre", {
       buffer = bufnr,
       callback = function()
         vim.lsp.buf.format();
+
+        if client.name == 'tsserver' or client.name == "svelte" then
+          -- params for the request
+          local params = {
+            command = "_typescript.organizeImports",
+            arguments = { vim.api.nvim_buf_get_name(bufnr) },
+            title = ""
+          }
+
+          -- perform a syncronous request
+          -- 500ms timeout depending on the size of file a bigger timeout may be needed
+          vim.lsp.buf_request_sync(bufnr, "workspace/executeCommand", params, 500)
+
+        end
+
       end
     })
   else
