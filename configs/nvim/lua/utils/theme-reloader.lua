28bc14372d46b0ad2defe9fe81cb849f9e3eff3e
Max <jim-x@web.de>
2022-06-23 00:24:06 +0200

    fix: error when not using theme file

--- configs/nvim/lua/utils/theme-reloader.lua
+++ configs/nvim/lua/utils/theme-reloader.lua
@@ -8,27 +8,26 @@ end
 -- get all lines from a file, returns an empty
 -- list/table if the file does not exist
 local function line_from(file)
-  if not file_exists(file) then return {} end
+  if not file_exists(file) then return "" end
   local f = io.open(file) -- 'r' is unnecessary because it's a default value.
+  if f == nil then return "" end
   local line = f:read() -- '*l' is unnecessary because it's a default value.
   f:close()
   return line;
 end
 
-local filePath = os.getenv("HOME").."/.cache/dark-mode";
+local filePath = os.getenv("HOME") .. "/.cache/dark-mode";
 local function updateTheme()
   local line = line_from(filePath);
   if line then
-    local dark = string.find(line, "dark");
-    if dark then
-      vim.cmd("colorscheme nightfox")
-    else
+    local light = string.find(line, "light");
+    if light then
       vim.cmd("colorscheme dayfox")
+    else
+      vim.cmd("colorscheme nightfox")
     end
   end
 end
-
-
 
 updateTheme()
 local w = vim.loop.new_fs_event()
