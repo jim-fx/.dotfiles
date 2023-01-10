8312963ee98c9440c361611fcaf71a13add5cb47
Max Richter <jim-x@web.de>
2023-01-10 16:12:52 +0100

    feat: some shit

--- configs/nvim/ftplugin/qf.lua
+++ configs/nvim/ftplugin/qf.lua
@@ -1,5 +1,5 @@
 -- Automatically close the goto definition buffer after leaving
-local buffer_id = vim.api.nvim_win_get_buf(0)
+-- local buffer_id = vim.api.nvim_win_get_buf(0)
 -- vim.api.nvim_create_autocmd({ "bufwinleave", "bufleave" }, {
 --   pattern = "*",
 --   once = true,
