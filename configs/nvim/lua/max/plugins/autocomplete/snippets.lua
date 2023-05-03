86bf6b21172b3c643e1f1c885a39c002408257b5
Max Richter <jim-x@web.de>
2023-05-03 09:21:04 +0200

    @

--- configs/nvim/lua/max/plugins/autocomplete/snippets.lua
+++ configs/nvim/lua/max/plugins/autocomplete/snippets.lua
@@ -6,8 +6,8 @@ local sn = ls.snippet_node
 local t = ls.text_node
 local i = ls.insert_node
 local f = ls.function_node
-local c = ls.choice_node
 local d = ls.dynamic_node
+local fmt = require("luasnip.extras.fmt").fmt
 
 local types = require("luasnip.util.types")
 
---
+++ configs/nvim/lua/max/plugins/neoai.lua
@@ -0,0 +1,28 @@
+return {
+  "Bryley/neoai.nvim",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+  },
+  cmd = {
+    "NeoAI",
+    "NeoAIOpen",
+    "NeoAIClose",
+    "NeoAIToggle",
+    "NeoAIContext",
+    "NeoAIContextOpen",
+    "NeoAIContextClose",
+    "NeoAIInject",
+    "NeoAIInjectCode",
+    "NeoAIInjectContext",
+    "NeoAIInjectContextCode",
+  },
+  keys = {
+    { "<leader>as", desc = "summarize text" },
+    { "<leader>ag", desc = "generate git message" },
+  },
+  config = function()
+    require("neoai").setup({
+      -- Options go here
+    })
+  end,
+}
