831c9389ad9043298a599631e91a49f7b4a1e35d
Max Richter <jim-x@web.de>
2023-10-11 16:56:18 +0200

    fix: install script

--- .dotfiles/install/asdf.sh
+++ .dotfiles/install/asdf.sh
@@ -1,12 +1,8 @@
 if [ -d ~/.asdf ] ; then
-
     echo " ✓ asdf already installed"
-
 else
-    
     git clone --quiet https://github.com/asdf-vm/asdf.git ~/.asdf > /dev/null
     cd ~/.asdf
     git checkout --quiet "$(git describe --abbrev=0 --tags)" > /dev/null
     . asdf.sh
-
 fi
