a97cc65d21be3256973cc90fe6579f6a31efaabb
Max Richter <max.richter@dynabase.de>
2024-08-29 13:37:01 +0200

    fix(nvim): disable eslint_d

--- .config/nvim/lua/max/plugins/lsp.lua
+++ .config/nvim/lua/max/plugins/lsp.lua
@@ -41,7 +41,6 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.eslintd,
         null_ls.builtins.formatting.phpcsfixer,
       },
     })
