00c568e7f616018e1963ae73273c8d6364cd255e
Max Richter <max.richter@dynabase.de>
2024-09-11 11:34:14 +0200

    feat: make run command also work with cargo, mvn and make

--- .config/zsh/functions/run.zsh
+++ .config/zsh/functions/run.zsh
@@ -1,7 +1,18 @@
 function run(){
-  if [[ -f "package.json" ]]; then
-    pnpm $1
-  elif [[ -f "Makefile" ]]; then
-    make $1
-  fi
+    if [ -f "pom.xml" ]; then
+        mvn "$@"
+    elif [ -f "pnpm-lock.yaml" ]; then
+        pnpm "$@"
+    elif [ -f "yarn.lock" ]; then
+        yarn "$@"
+    elif [ -f "Cargo.toml" ]; then
+      cargo "$@"
+    elif [ -f "package-lock.json" ]; then
+        npm "$@"
+    elif [ -f "Makefile" ]; then
+        make $1
+    else
+        echo "No known package manager detected."
+        return 1
+    fi
 }
