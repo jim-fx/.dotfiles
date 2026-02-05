d0f74aa724a5097ef03d4010864cde3b5c7bfd88
Max Richter <max@max-richter.dev>
2026-02-05 23:30:55 +0100

    update

--- .config/lazyvim/lua/plugins/treesitter.lua
+++ .config/lazyvim/lua/plugins/treesitter.lua
@@ -1,11 +1,10 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPost", "BufNewFile" },
-    opts = {
-      ensure_installed = nil, -- nothing preinstalled
-      auto_install = true, -- install when buffer language is detected
-      highlight = { enable = true },
-    },
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPost", "BufNewFile" },
+  opts_extend = {},
+  opts = {
+    ensure_installed = {}, -- nothing preinstalled
+    auto_install = true, -- install when buffer language is detected
+    highlight = { enable = true },
   },
 }
